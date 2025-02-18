1303ee61c8eed2130e851509c37099d9b5d35578
Nazar Palamarchuk <ua.nazar.palamarchuk.ua@gmail.com>
2025-02-18 22:32:45 +0200

    Replace quotes with HTML entity in client component descriptions

--- components/with-suspense-and-use/child.client.tsx
+++ components/with-suspense-and-use/child.client.tsx
@@ -7,7 +7,7 @@ export function Child({ dataPromise }: { dataPromise: Promise<string> }) {
   return (
     <div className="flex-1 bg-white/50 border border-slate-200 p-4 rounded-lg text-slate-800 space-y-2">
       <h3 className="text-lg font-semibold ">Child Client Component</h3>
-      <p>Client component that uses the "use" api</p>
+      <p>Client component that uses the &quot;use&quot; api</p>
       <p>{data}</p>
     </div>
   )
--- components/with-suspense/grandchild.client.tsx
+++ components/with-suspense/grandchild.client.tsx
@@ -4,7 +4,7 @@ export function Grandchild({ data }: { data: string }) {
   return (
     <div className="flex-1 bg-white/50 border border-slate-200 p-4 rounded-lg text-slate-800 space-y-2">
       <h3 className="text-lg font-semibold ">Grandchild Client Component</h3>
-      <p>Client component that does not use the "use" api</p>
+      <p>Client component that does not use the &quot;use&quot; api</p>
       <p>{data}</p>
     </div>
   )
